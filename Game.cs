0e9741de21e52ba8e1be6743728cf076a74313b6
Daniil Shevtsov <daniil.shevtsov@aviasales.ru>
2024-01-20 09:07:55 +0400

    place paddle in the center of the screen

--- Game.cs
+++ Game.cs
@@ -4,12 +4,23 @@ using System;
 public partial class Game : Node2D
 {
     Paddle paddle = null;
+    Area2D fieldArea = null;
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
         paddle = GetNode<Paddle>("Paddle");
+        fieldArea = GetNode<Area2D>("FieldArea");
+
         GD.Print(paddle);
+        GD.Print(fieldArea);
+
+        paddle.GlobalPosition = new Vector2(
+            ((RectangleShape2D)fieldArea.GetNode<CollisionShape2D>("CollisionShape2D").Shape).Size.X
+                / 2f,
+            ((RectangleShape2D)fieldArea.GetNode<CollisionShape2D>("CollisionShape2D").Shape).Size.Y
+                / 2f
+        );
     }
 
     // Called every frame. 'delta' is the elapsed time since the previous frame.
--- Paddle.cs
+++ Paddle.cs
@@ -3,8 +3,14 @@ using System;
 
 public partial class Paddle : StaticBody2D
 {
+    CollisionShape2D collisionShape = null;
+
     // Called when the node enters the scene tree for the first time.
-    public override void _Ready() { }
+    public override void _Ready()
+    {
+        collisionShape = GetNode<CollisionShape2D>("CollisionShape2D");
+        GD.Print(collisionShape);
+    }
 
     // Called every frame. 'delta' is the elapsed time since the previous frame.
     public override void _Process(double delta) { }
